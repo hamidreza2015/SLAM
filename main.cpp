ea715166cb5f59c8ddefd0c1770923e3bf2a370a
hamidreza2015 <hamidrezahosseinpoor@gmail.com>
2024-04-23 18:45:54 +0330

    add new ine for test

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ int main()
 {
     std::cout<<"test git"<<std::endl;
     std::cout<<"test git hamidreza"<<std::endl;
+    std::cout<<"test git hamidreza for new learn"<<std::endl;
 
     return 0 ;
 }
